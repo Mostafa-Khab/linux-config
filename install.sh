35b7b1034f9ed55228598019518ec2a58790fa18
Most Khal <mostafa6khaled@gmail.com>
2024-04-26 22:36:23 +0300

    updating install script (ftplugin and zathurarc added

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@
 #some apps needs other apps like vifm which you can find in vifmrc in programs section
 
 #set TERM variable to screen-256color (xterm-256color in case you use xterm)
+#ftplugin depends on someprograms as (pdflatex (tex files) and pandoc (for markdown) )
 
 cp .vimrc .bashrc .tmux.conf $HOME
 cp vifmrc $HOME/.config/vifm/
 cp config.fish $HOME/.config/fish
 cp -r picom.conf i3/ i3status/ oh-my-posh-theme/ $HOME/.config
+cp zathurarc $HOME/.config/zathura/
+cp -r ftplugin/ $HOME/.vim/ftplugin/
 
 #uncomment if you use xterm as your terminal
 #xrdb -merge .Xresources
--- safe-install.sh
+++ safe-install.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-#remember to install (vim vifm picom fish i3 tmux)
+#remember to install (vim vifm picom fish i3 tmux zathura)
 #you can use (sudo apt install vim vifm picom fish i3 tmux)
 #this configuration uses xterm as primary terminal with gruvbox like colorscheme
 #some apps needs other apps like vifm which you can find in vifmrc in programs section
 
 #set TERM variable to screen-256color (xterm-256color in case you use xterm)
+#ftplugin depends on someprograms as (pdflatex (tex files) and pandoc (for markdown) )
 
 cp -n .vimrc .bashrc .tmux.conf $HOME
-cp -n ftplugin/cpp.vim ftplugin/tex.vim $HOME/.vim/ftplugin/
 cp -n vifmrc $HOME/.config/vifm/
 cp -n config.fish $HOME/.config/fish
 cp -n -r picom.conf i3/ i3status/ oh-my-posh-theme/ $HOME/.config
+cp -n zathurarc $HOME/.config/zathura
+cp -n -r ftplugin/ $HOME/.vim/ftplugin/
 
 #uncomment if you use xterm as your terminal
 #xrdb -merge .Xresources
