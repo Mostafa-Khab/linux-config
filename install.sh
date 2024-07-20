f637e5519a43516b59c6504b0ba88de565467f00
Most Khal <mostafa6khaled@gmail.com>
2024-07-20 22:33:20 +0300

    update install scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-#remember to install (vim vifm picom fish i3 tmux)
-#you can use (sudo apt install vim vifm picom fish i3 tmux)
-#this configuration uses xterm as primary terminal with gruvbox like colorscheme
-#some apps needs other apps like vifm which you can find in vifmrc in programs section
-
-#set TERM variable to screen-256color (xterm-256color in case you use xterm)
-#ftplugin depends on someprograms as (pdflatex (tex files) and pandoc (for markdown) )
+# remember to install (vim vifm picom fish i3 tmux)
+# you can use (sudo apt install vim vifm picom fish i3 tmux)
+# this configuration uses xterm as primary terminal with gruvbox like colorscheme
+# some apps needs other apps like vifm which you can find in vifmrc in programs section
+  
+# set TERM variable to screen-256color (xterm-256color in case you use xterm)
+# ftplugin depends on some programs as (pdflatex (tex files) and pandoc (for markdown) )
+ 
+# if you know what to do, it's better to configure your system with the help of these files (not install scripts)
 
 cp .vimrc .bashrc .tmux.conf $HOME
 cp vifmrc $HOME/.config/vifm/
@@ -14,16 +16,7 @@ cp config.fish $HOME/.config/fish
 cp -r picom.conf i3/ i3status/ oh-my-posh-theme/ $HOME/.config
 cp zathurarc $HOME/.config/zathura/
 cp -r ftplugin/ $HOME/.vim/ftplugin/
+cp scripts/* /usr/local/bin
 
 #uncomment if you use xterm as your terminal
 #xrdb -merge .Xresources
-
-#compile main.cpp in exists as exists (g++ main.cpp -o exists)
-#compile main.cpp in git-branch as git-prompt (used by config.fish)
-#install both of them to /usr/local/bin (you can uncomment the 2 lines under this one)
-#install exists \usr\loca\bin
-#install git-prompt \usr\loca\bin
-
-#if you have oh my posh
-#cp -r oh-my-posh-theme $HOME/.config/
-
--- safe-install.sh
+++ safe-install.sh
@@ -1,29 +1,22 @@
 #!/bin/sh
 
-#remember to install (vim vifm picom fish i3 tmux zathura)
-#you can use (sudo apt install vim vifm picom fish i3 tmux)
-#this configuration uses xterm as primary terminal with gruvbox like colorscheme
-#some apps needs other apps like vifm which you can find in vifmrc in programs section
-
-#set TERM variable to screen-256color (xterm-256color in case you use xterm)
-#ftplugin depends on someprograms as (pdflatex (tex files) and pandoc (for markdown) )
+# remember to install (vim vifm picom fish i3 tmux)
+# you can use (sudo apt install vim vifm picom fish i3 tmux)
+# this configuration uses xterm as primary terminal with gruvbox like colorscheme
+# some apps needs other apps like vifm which you can find in vifmrc in programs section
+  
+# set TERM variable to screen-256color (xterm-256color in case you use xterm)
+# ftplugin depends on some programs as (pdflatex (tex files) and pandoc (for markdown) )
+ 
+# if you know what to do, it's better to configure your system with the help of these files (not install scripts)
 
 cp -n .vimrc .bashrc .tmux.conf $HOME
 cp -n vifmrc $HOME/.config/vifm/
 cp -n config.fish $HOME/.config/fish
 cp -n -r picom.conf i3/ i3status/ oh-my-posh-theme/ $HOME/.config
-cp -n zathurarc $HOME/.config/zathura
+cp -n zathurarc $HOME/.config/zathura/
 cp -n -r ftplugin/ $HOME/.vim/ftplugin/
+cp -n scripts/* /usr/local/bin
 
 #uncomment if you use xterm as your terminal
 #xrdb -merge .Xresources
-
-#compile main.cpp in exists as exists (g++ main.cpp -o exists)
-#compile main.cpp in git-branch as git-prompt (used by config.fish)
-#install both of them to /usr/local/bin (you can uncomment the 2 lines under this one)
-#install exists \usr\loca\bin
-#install git-prompt \usr\loca\bin
-
-#if you have oh my posh
-#cp -n -r oh-my-posh-theme $HOME/.config/
-
